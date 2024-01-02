9a88d7a503b29aa9bec3994798712ac65b3a11ce
Fernando Ferreira Rocha <fernandofr510@gmail.com>
2024-01-02 19:02:54 -0300

    feat: value object to slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,29 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   *
+   * @param text
+   *
+   * Receives a string and normalize it as a slug.
+   *
+   * Example: "an example title" => 'an-example-title
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '-');
+
+    return new Slug(slugText);
+  }
+}
