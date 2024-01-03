6c943ca2d273a4650e5a3be2269406044e4a0590
Fernando Ferreira Rocha <fernandofr510@gmail.com>
2024-01-03 19:05:02 -0300

    feat: mapping props

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,16 @@
 import { Entity } from '../../core/entities/entity';
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
+
 import { Slug } from './value-objects/slug';
 
 interface QuestionsProps {
+  authorId: UniqueEntityID;
   title: string;
   content: string;
-  authorId: string;
   slug: Slug;
+  bestAnswerId?: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionsProps> {}
