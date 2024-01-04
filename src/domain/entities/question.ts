f3bc828055ad625f909e300c12866555ef0b7f06
Fernando Ferreira Rocha <fernandofr510@gmail.com>
2024-01-04 19:06:38 -0300

    feat: getters and setters

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { Entity } from '../../core/entities/entity';
 import { UniqueEntityID } from '../../core/entities/unique-entity-id';
 import { Optional } from '../../core/types/optional';
@@ -15,11 +16,75 @@ interface QuestionsProps {
 }
 
 export class Question extends Entity<QuestionsProps> {
-  static create(props: Optional<QuestionsProps, 'createdAt'>, id?: string) {
-    const question = new Question({
-      ...props,
-      createdAt: new Date()
-    });
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...');
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+
+    this.touch();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  static create(
+    props: Optional<QuestionsProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date()
+      },
+      id
+    );
 
     return question;
   }
